ab5e0f6c2992740afb2070668cc257d1298f0ff4
Jordan Wang <jordanwang@Jordans-Laptop-2.local>
2026-05-11 19:14:21 -0400

    changes made to rnn_text_generation and rnn

--- Project 4 - Recurrent Neural Networks/rnn.py
+++ Project 4 - Recurrent Neural Networks/rnn.py
@@ -186,7 +186,7 @@ class RNN(network.DeepNetwork):
  
         return net_act, tuple(rec_layer_states)
 
-    @tf.function
+    @tf.function(jit_compile=True)
     def train_step(self, x_batch, y_batch):
         '''Completely process a single mini-batch of data during training. This includes:
         1. Performing a forward pass of the data through the entire network.
@@ -218,7 +218,7 @@ class RNN(network.DeepNetwork):
         self.update_params(tape, loss)
         return loss
 
-    @tf.function
+    @tf.function(jit_compile=True)
     def test_step(self, x_batch, y_batch):
         '''Completely process a single mini-batch of data during test/validation time. This includes:
         1. Performing a forward pass of the data through the entire network.
